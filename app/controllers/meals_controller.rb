6fa63f672c092610c3a341fe17219b8ea5c511d3
bonbo36 <shino.yamane99@gmail.com>
2023-04-14 18:38:28 +0900

    フロント実装

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,4 +1,9 @@
 class MealsController < ApplicationController
   def index
+    @meals = Meal.all
+  end
+
+  def new
+    @meal = Meal.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'meals/index'
 root to: "meals#index"
+resources :meals, only: [:index, :new]
 end
